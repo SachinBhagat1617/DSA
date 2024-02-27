cfe75abbbef1c462acb0920b9e8196b3d9b896b9
Sachin Bhagat <99602496+SachinBhagat1617@users.noreply.github.com>
2024-02-27 14:10:42 +0530

    Add files via upload

---
+++ hashing/atlassian OA session 29.cpp
@@ -0,0 +1,34 @@
+//atlassian OA
+//hashing session 29
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // analyse the equation
+    //=>s[i]-s[j]=i-j (condition for balance)
+    //=>s[i]-i=s[j]-j
+    // means we have to find the largest sum of subsequence having s[i]-i=s[j]-j=s[k]-k
+    
+    //eg: arr=[1,5,3,7,8]
+//no.thatcanbalance (1-0),(5-1),(3-2),(7-3),(8-4)
+           //=[1,4,1,4,4]
+           //mp[1]=1+3  mp[4]=5+7+8=20
+    
+    unordered_map<int,int>mp;
+    int maxi=-1e9;
+    for(int i=0;i<n;i++){
+        int b=arr[i]-i;
+        mp[b]+=arr[i];
+        maxi=max(maxi,mp[b]);
+    }
+    cout<<maxi<<endl;
+    
+    
+}
